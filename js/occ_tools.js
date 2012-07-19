ab96f202b47119e3512b9dd42808ecf3837c19e0
gildedrain <steve@petrock.org>
2012-07-19 13:04:28 -0400

    new site header drawer js,css,imgs

--- js/occ_tools.js
+++ js/occ_tools.js
@@ -3,10 +3,10 @@
 // --------------------------------------------------------------------------------------//
 $(document).ready(function() {
 
-// Animate #panel open & close
+// Animate quicklinks #panel open & close
     $('#panel').removeClass('basic'); // JQuery takes over, #panel.basic:hover-to-show is removed
     $('#panel').addClass('closed');
-    $('#toggle_open').click(function() {  
+    $('#toggle_open').click(function() {
       $('#subpanel').animate({height:'show'}, 300, function() {
         $('#panel').removeClass('closed');
         $('#panel').addClass('opened');
@@ -38,4 +38,56 @@ $(document).ready(function() {
         window.location.href="http://admission.sunyocc.edu/search.aspx?cx=009247214289687445570%3As8i2fdwbink&cof=FORID%3A11&ie=UTF-8&sa=Search&q=" + qHold;
       }
     };
+});
+
+
+// Open/Close Animation for Site Header Navigation "Drawer"
+$(document).ready(function () {
+  // disable basic functionality
+  $('#occheader, .headernav').removeClass('basic');
+
+  // trackers & units
+  var showHeader = false; // Initially, it SHOULD NOT be shown
+  var visible = false; // Initially, it IS NOT shown
+  var hideDelay = 750;
+  var hideDelayTimer = null;
+  var openHeight = 275;
+  var closedHeight = 48;
+
+  // mouseenter event opens drawer
+  $('#home_top').mouseenter(function() {
+    if (hideDelayTimer) clearTimeout(hideDelayTimer);
+    if (visible == true) { // don't do anything if it's already visible
+      return;
+    }
+    else {
+      showHeader = true;
+      if (showHeader == true && visible == false) {
+        headerOpen();
+      }
+    }
+  });
+
+  // mouseleave event closes drawer after a delay
+  $('#home_top').mouseleave(function() {
+    // reset the timer if we get fired again - avoids double animations
+    if (hideDelayTimer) clearTimeout(hideDelayTimer);
+    // store the timer so that it can be cleared in the mouseover if required
+    hideDelayTimer = setTimeout(function () {
+      hideDelayTimer = null;
+      showHeader = false;
+      if (showHeader == false && visible == true) {
+        headerClose();
+      }
+    }, hideDelay);
+  });
+
+  function headerOpen () {
+    $('#occheader, .headernav').animate({height: openHeight + 'px'}, 150);
+    visible = true;
+  }
+  function headerClose () {
+    $('#occheader, .headernav').animate({height: closedHeight + 'px',}, 150);
+    visible = false;
+  }
 });
