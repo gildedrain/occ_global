bb108e87d7fa72bd743c32285874c8e04a97f783
gildedrain <steve@petrock.org>
2012-07-26 15:13:15 -0400

    added new siteheader drawer

--- js/occ_tools.js
+++ js/occ_tools.js
@@ -19,39 +19,17 @@ $(document).ready(function() {
       });
     });
 
-// Take current Google Search query and use it to search within the scope of a different website
-// to add a new option, modify the subdomain / subdirectory, and modify the CXkey (created on google webmaster tools)
-  occSwitchSearch = function(NewSite) {
-    loc = window.location.href;
-    qkey = parseUri(loc).queryKey;
-    qHold = $(qkey).attr("q");
-    if (NewSite == 'www') {
-      window.location.href="http://www.sunyocc.edu/search.aspx?cx=009247214289687445570%3A4z5fbtg2lek&cof=FORID%3A11&ie=UTF-8&sa=Search&q=" + qHold;
-    }
-    else if (NewSite == 'wwwold') {
-      window.location.href="http://www.sunyocc.edu/searchresult.aspx?cx=009247214289687445570%3A4z5fbtg2lek&cof=FORID%3A11&ie=UTF-8&sa=Search&q=" + qHold;
-    }
-    else if (NewSite == 'students') {
-      window.location.href="http://students.sunyocc.edu/search.aspx?cx=012429996574113949521%3Az7uv6wcw71e&cof=FORID%3A11&ie=UTF-8&sa=Search&q=" + qHold;
-    }
-    else if (NewSite == 'admission') {
-      window.location.href="http://admission.sunyocc.edu/search.aspx?cx=009247214289687445570%3As8i2fdwbink&cof=FORID%3A11&ie=UTF-8&sa=Search&q=" + qHold;
-    }
-  };
-
-
-
 // Open/Close Animation for Site Header Navigation "Drawer"
+// requires JQUERY UI EFFECTS CORE - but only for .switchClass()
   // disable basic functionality
   $('#occheaderbg, .headernav').removeClass('basic');
+  $('#occheaderbg, .headernav').addClass('closed');
 
   // trackers & units
   var showHeader = false; // Initially, it SHOULD NOT be shown
   var visible = false; // Initially, it IS NOT shown
   var hideDelay = 750;
   var hideDelayTimer = null;
-  var openHeight = 275;
-  var closedHeight = 48;
 
   // mouseenter event opens drawer
   $('#home_top').mouseenter(function() {
@@ -82,11 +60,11 @@ $(document).ready(function() {
   });
 
   function headerOpen () {
-    $('#occheaderbg, .headernav').animate({height: openHeight + 'px'}, 150);
+    $('#occheaderbg, .headernav').switchClass('closed', 'opened', 150);
     visible = true;
   }
   function headerClose () {
-    $('#occheaderbg, .headernav').animate({height: closedHeight + 'px'}, 150);
+    $('#occheaderbg, .headernav').switchClass('opened', 'closed', 150);
     visible = false;
   }
 });
